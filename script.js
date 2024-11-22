ac375e180b52809205758b27eb7de7a2bcdd593c
LimeFrogg <bellamones@icloud.com>
2024-11-21 21:54:53 -0500

    Question 2

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ let characters = ['Harry', 'Hermione', 'Ron', 'Draco', 'Luna'];
 for (let i = 0; i < characters.length; i++) {
   console.log(characters[i]);
 }
+
+let firstName = 'Harry';
+let house = 'Gryffindor';
+console.log(`Welcome, ${firstName} of ${house}!`);
